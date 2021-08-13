29aaeab0283e59676c24d03f39e8acbb2f284837
goyalvinay <vinay.goyal@exactspace.co>
2021-08-13 17:23:39 +0530

    Working Version

--- empty.sh
+++ empty.sh
@@ -1,16 +1,7 @@
 #! /bin/bash
-a=$(grep -l ".log" *)
-echo $a
-echo
 for line in $(ls -S);do
-  echo $line
-  echo "Do you want to truncate any file?"
-   read c
-  if [ "$c" -eq "y" -o "$c" -eq "yes" ]; then
-    read filename
-    var1=$(truncate -s 0 "$filename".sh)
-  fi
-
+  a=$(find .  -name \*.log)
+  echo $a
 done 
 
 
